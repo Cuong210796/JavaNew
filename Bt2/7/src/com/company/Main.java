56b14cf2737c97583b699a2a589620a35bb8aa47
cuong <chuchicuong>
2020-09-20 19:20:38 +0700

    java

--- Bt2/7/src/com/company/Main.java
+++ Bt2/7/src/com/company/Main.java
@@ -8,27 +8,21 @@ public class Main {
 	/*7. Viết chương trình cho phép nhập vào từ bàn phím toạ độ x, y và chiều dài, chiều rộng
 	 của hai hình chữ nhật, sau đó kiểm tra xem chúng giao nhau hay bọc lấy nhau hay nằm ngoài nhau.*/
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap toa do x1 cua HCN1 = ");
-        double x1 = sc.nextDouble();
-        System.out.println("Nhap toa do y1 cua HCN1 = ");
-        double y1 = sc.nextDouble();
-        System.out.println("Nhap chieu dai cua HCN1 = ");
-        double d1 = sc.nextDouble();
-        System.out.println("Nhap chieu rong cua HCN1 = ");
-        double r1 = sc.nextDouble();
-        System.out.println("Nhap toa do x2 cua HCN2 = ");
-        double x2 = sc.nextDouble();
-        System.out.println("Nhap toa do y2 cua HCN2 = ");
-        double y2 = sc.nextDouble();
-        System.out.println("Nhap chieu dai cua HCN2 = ");
-        double d2 = sc.nextDouble();
-        System.out.println("Nhap chieu rong cua HCN2 = ");
-        double r2 = sc.nextDouble();
-        if ((x1+r1>=x2)&&(x2+r2>=x1)&&(y1+d1>=y2)&&(y2+d2>=y1)){
-         System.out.println("2 HCN giao nhau");
-        }
-        else {
-            System.out.println("2 HCN không giao nhau");
-        }
+        System.out.println("nhap cac so nguyen x1,y1,w1,h1 :");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int w1= sc.nextInt();
+        int h1 = sc.nextInt();
+        System.out.println("nhap cac so nguyen x2,y2,w2,h2 :");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int w2 = sc.nextInt();
+        int h2 = sc.nextInt();
+
+        var rectangle1 = new Rectangle(x1, y1, w1, h1);
+        var rectangle2 = new Rectangle(x2, y2, w2, h2);
+
+        String result = Condition.rectangleCompare(rectangle1, rectangle2);
+        System.out.println(result);
     }
 }
